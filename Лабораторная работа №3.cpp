e7c36cbc5fbb86e3328c01d644d4fd9934f8e733
Руслан Вальшин <Ruslan376@outlook.com>
2020-11-06 12:30:20 +0500

    Добавлен класс MaterialObject

--- Лабораторная работа №3.cpp
+++ Лабораторная работа №3.cpp
@@ -1,7 +1,46 @@
 ﻿#include <iostream>
 
+class MaterialObject
+{
+	double mass; // В граммах
+	double volume; // В кубических сантиметрах
+
+	MaterialObject()
+	{
+		mass = 0;
+		volume = 0;
+		printf("Создан объект MaterialObject, mass = %f, volume = %f\n", mass, volume);
+	}
+
+	MaterialObject(double mass, double volume)
+	{
+		this->mass = mass;
+		this->volume = volume;
+		printf("Создан объект MaterialObject, mass = %f, volume = %f\n", mass, volume);
+	}
+
+	MaterialObject(MaterialObject& object)
+	{
+		mass = object.mass;
+		volume = object.volume;
+		printf("Скопирован объект MaterialObject, mass = %f, volume = %f\n", mass, volume);
+	}
+
+	virtual void message()
+	{
+		printf("Этот объект принадлежит классу MaterialObject\n");
+	}
+
+	virtual ~MaterialObject()
+	{
+		printf("Удаление объекта MaterialObject, mass = %f, volume = %f\n", mass, volume);
+	}
+};
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	
 }
+
+
 
